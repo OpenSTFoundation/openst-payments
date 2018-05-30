87266004a00d5f608319f76caf8524f5c851fec2
abhay-pepo <abhay@pepo.com>
2018-05-30 18:16:18 +0530

    Contract updated to test multiple SIHs

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -2,14 +2,17 @@
 pragma solidity ^0.4.23;
 
 contract Storage {
-    uint public pos0;
-    mapping(address => uint) public pos1;
-    address public addr1 = 0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8;
-    address public addr2 = 0x2a65Aca4D5fC5B5C859090a6c34d164135398226;
+    event Test(bytes32 sih1, bytes32 sih2);
+
+    string public pos0;
+    mapping(bytes32 => string) public pos1;
+
+    bytes32 public sih1 = 0x14bb2bf372bbfc1de82d7a80510e8bf9c0735e1982c822f370f0882fc1d4f607;
+    bytes32 public sih2 = 0xa20cef82a08a0952b1989262b53492c68a64dc230a885aff8c38dc9bd067a8d0;
 
     constructor() public {
-        pos0 = 1234;
-        pos1[addr1] = 5678;
-        pos1[addr2] = 5680;
+        pos0 = '1234';
+        pos1[sih1] = '5678';
+        pos1[sih2] = '5680';
     }
 }
