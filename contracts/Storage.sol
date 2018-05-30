4a70942cefe40f06d0d4fa5f4e85a9387fb44959
abhay-pepo <abhay@pepo.com>
2018-05-30 16:30:42 +0530

    Updated abi and bin files Storage.sol with multiple SIHs

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,12 +1,15 @@
 /* solhint-disable-next-line compiler-fixed */
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.23;
 
 contract Storage {
     uint public pos0;
     mapping(address => uint) public pos1;
+    address public addr1 = 0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8;
+    address public addr2 = 0x2a65Aca4D5fC5B5C859090a6c34d164135398226;
 
-    function Storage() public {
+    constructor() public {
         pos0 = 1234;
-        pos1[msg.sender] = 5678;
+        pos1[addr1] = 5678;
+        pos1[addr2] = 5680;
     }
 }
